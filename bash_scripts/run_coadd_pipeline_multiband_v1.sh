0da6fb6beb39baceaecddf180f29035a8483a3db
psferguson <petersferguson@gmail.com>
2023-09-27 15:18:05 +0000

    draft of plots

--- bash_scripts/run_coadd_pipeline_multiband_v1.sh
+++ bash_scripts/run_coadd_pipeline_multiband_v1.sh
@@ -1,30 +1,21 @@
 DATA_COLL=HSC/runs/RC2/w_2023_32/DM-40356
-INJECT_COLL=u/pferguso/full_dwarf_test/1_lim_32
 REPO=/repo/main
-OUTPUT=u/pferguso/full_dwarf_test/1_lim_32/step3
-pipetask --long-log \
-run --register-dataset-types \
---instrument lsst.obs.subaru.HyperSuprimeCam \
--b $REPO \
--i $DATA_COLL,$INJECT_COLL \
--o $OUTPUT \
--p pipelines/DRP-RC2+injection_by_peter.yaml#step3 \
--d "instrument='HSC' AND skymap='hsc_rings_v1' AND
-tract=9615 AND patch=3" \
--j 3
 
+names='27 29 30 31 33'
+for name in $names
+do
+    INJECT_COLL=u/pferguso/full_dwarf_test/2_lim_$name
+    OUTPUT=u/pferguso/full_dwarf_test/2_lim_$name/step3
+    echo $INJECT_COLL
+    pipetask --long-log \
+    run --register-dataset-types \
+    --instrument lsst.obs.subaru.HyperSuprimeCam \
+    -b $REPO \
+    -i $DATA_COLL,$INJECT_COLL \
+    -o $OUTPUT \
+    -p pipelines/DRP-RC2+injection_by_peter.yaml#step3 \
+    -d "instrument='HSC' AND skymap='hsc_rings_v1' AND
+    tract=9615 AND patch=3 AND (band = 'g' OR band = 'r' OR band = 'i') " \
+    -j 6
+done
 
-DATA_COLL=HSC/runs/RC2/w_2023_32/DM-40356
-INJECT_COLL=u/pferguso/full_dwarf_test/1_lim_28
-REPO=/repo/main
-OUTPUT=u/pferguso/full_dwarf_test/1_lim_28/step3
-pipetask --long-log \
-run --register-dataset-types \
---instrument lsst.obs.subaru.HyperSuprimeCam \
--b $REPO \
--i $DATA_COLL,$INJECT_COLL \
--o $OUTPUT \
--p pipelines/DRP-RC2+injection_by_peter.yaml#step3 \
--d "instrument='HSC' AND skymap='hsc_rings_v1' AND
-tract=9615 AND patch=3" \
--j 3
