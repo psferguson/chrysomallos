6cf99acd416600180bd4312e377e71006ff8ff96
psferguson <petersferguson@gmail.com>
2023-10-09 11:24:35 -0700

    docstrings!

--- python/starlink/synthpop/create_and_injest_a_cat.py
+++ python/starlink/synthpop/create_and_injest_a_cat.py
@@ -20,43 +20,82 @@ default_config_dict={
     "bands" : ["g","r","i"],
     "dwarfs": []
     }
-default_dwarf_dict = {
-    "id": 0,
-    "x_cen": None,
-    "y_cen": None,
-    "age": 10.0,
-    "feh": -2.0,
-    "mass": 5.0e6,
-    "dist": 2.0,
-    "r_scale": 100,
-    "ellip": 0,
-    "theta": 0,
-    "n": 1,
-    "mag_limit": 27,
-    "mag_limit_band": "LSST_g",
-    "random_seed": None
-}
+
 class DwarfConfig():
+     """
+    Configuration class for a dwarf galaxy. 
+    Sets various properties of the dwarf galaxy, including its location, 
+    age, metallicity, and other parameters.
+    """
+    # DEFAULT PARAMETERS 
+    # ID of the injection cat
+    id = None
+    # X-coordinate of the object's center [pixel]
+    x_cen = None
+    # Y-coordinate of the object's center [pixel]
+    y_cen = None
+    # Age of the object [Gyr]
+    age = 10.0
+    # Metallicity of the object [Fe/H]
+    feh = -2.0
+    # Mass of the object [Msolar]
+    mass = 5.0e5
+    # Distance to the object [Mpc]
+    dist = 2.0 
+    # Scale radius of the object [pc]
+    r_scale = 100 
+    # Ellipticity of the object (0-1)
+    ellip = 0
+    # Orientation angle of the object
+    theta = 0
+    # Sersic index of the object (shape parameter)
+    n = 1
+    # Magnitude limit to inject for the object
+    mag_limit = 27
+    # Band in which the magnitude limit is defined
+    mag_limit_band = "LSST_g"
+    # Seed for random number generation (for reproducibility)
+    random_seed = None
+    
     def __init__(self, dwarf_dict):
-        #set defaults:
-        config_dict = default_dwarf_dict.copy()
+        """
+        Initialize the class with the given dwarf dictionary.
         
+        Parameters
+        ----------
+        dwarf_dict : dict
+            Dictionary containing the properties and attributes of a dwarf.
+        """
         # update new values
         for key, value in dwarf_dict.items():
-            config_dict[key] = value
-       
-        for key, value in config_dict.items():
             setattr(self, key, value)
+            
         
 
 class CreateDwarfInjectionCatalog():
-    """ some stuff here """
+    """
+    Class to create an injection catalog for dwarf galaxies.
+    Processes configuration data, fetches coadd from butler, 
+    and creates catalogs ready for injection, then ingests into butler.
+    """
     def __init__(self, config_dict=default_config_dict):
+        """
+        Initialize the class with the given configuration dictionary.
+        
+        Parameters
+        ----------
+        config_dict : dict, optional
+            Configuration dictionary with parameters and settings.
+            Default is `default_config_dict`.
+        """
         self.config_dict = config_dict
         for key, value in self.config_dict.items():
             setattr(self, key, value)
     
     def run(self):
+         """
+        Main method to run the dwarf injection catalog creation process.
+        """
         self.get_data_ids()
         self.butler = Butler(self.repo, collections=self.collection)
         self.get_coadds()
@@ -103,14 +142,28 @@ class CreateDwarfInjectionCatalog():
         
         
     def get_data_ids(self, tract=9615, patch=3):
+        """
+        Get data IDs based on band, skymap, tract, and patch information.
+        
+        Parameters
+        ----------
+        tract : int, optional
+            Tract number. Default is 9615.
+        patch : int, optional
+            Patch number. Default is 3.
+        """
         self.dataid_dict = {}
         for band in self.bands:
             self.dataid_dict[band] = {'band': band, 'skymap': 'hsc_rings_v1', 'tract': tract, 'patch': patch}
     
     def get_coadds(self):
+        """
+        Fetch coadd data based on the specified bands and data IDs.
+        """
         self.coadd_dict = {}
         for band in self.bands: 
             n_dataid=len(self.dataid_dict[band])
+            # currently this only grabs one coadd per band? 
             # if  n_dataid > 1:
             #     msg = f'{n_dataid} calexp data ids in {band}-band only using first one'
             #     logger.warning(msg)
@@ -119,6 +172,18 @@ class CreateDwarfInjectionCatalog():
         self.bbox = self.coadd_dict['g'].getBBox()
     
     def ingest_injection_catalogs(self, si_input_collection, catalogs, bands):
+        """
+        Ingest the prepared injection catalogs into the specified collection.
+        
+        Parameters
+        ----------
+        si_input_collection : str
+            The collection to ingest the catalogs into.
+        catalogs : dict
+            Dictionary of prepared catalogs, keyed by band.
+        bands : list
+            List of bands to consider.
+        """
         writeable_butler =  Butler(self.repo, writeable=True)
         try:
             writeable_butler.removeRuns([si_input_collection])
@@ -140,6 +205,11 @@ class CreateDwarfInjectionCatalog():
             )
     
 if __name__ == "__main__":
+    """
+    Main execution script. 
+    Parses input arguments, reads the configuration file, 
+    and runs the dwarf injection catalog creation.
+    """
     config_dict = default_config_dict.copy()
     parser = argparse.ArgumentParser(description="Description of your script/functionality.")
     parser.add_argument("filename", help="name of config.yaml file")
