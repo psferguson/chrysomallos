104a1e0ababaad1344e611362a119f16d31f7b04
psferguson <petersferguson@gmail.com>
2023-11-16 12:29:53 -0800

    pre-commit

--- bin/create_and_ingest_from_config.py
+++ bin/create_and_ingest_from_config.py
@@ -1,18 +1,8 @@
 import argparse
-import astropy.table as atable
-import astropy.units as u
-import numpy as np
-from tqdm import tqdm
+
 import yaml
-from dataclasses import dataclass
 
-
-from lsst.daf.butler import Butler, CollectionType
-from lsst.daf.butler.registry import MissingCollectionError
-import lsst.source.injection as si
-
-from chrysomallos.synthpop import default_config_dict, CreateDwarfInjectionCatalog
-from chrysomallos.utils.log import logger
+from chrysomallos.synthpop import CreateDwarfInjectionCatalog, default_config_dict
 
 if __name__ == "__main__":
     """
@@ -21,18 +11,25 @@ if __name__ == "__main__":
     and runs the dwarf injection catalog creation.
     """
     config_dict = default_config_dict.copy()
-    parser = argparse.ArgumentParser(description="Description of your script/functionality.")
+    parser = argparse.ArgumentParser(
+        description="Description of your script/functionality."
+    )
     parser.add_argument("filename", help="name of config.yaml file")
-    parser.add_argument("--i","-ingest", default=False, action="store_true",
-                        help="ingest catalogs into butler")
+    parser.add_argument(
+        "-ingest",
+        "--i",
+        default=False,
+        action="store_true",
+        help="ingest catalogs into butler",
+    )
 
     args = parser.parse_args()
+    ingest_bool = args.i
 
     with open(args.filename, "r") as file:
         user_config_dict = yaml.safe_load(file)
         for key, value in user_config_dict.items():
             config_dict[key] = value
 
-
-    creator=CreateDwarfInjectionCatalog(config_dict)
-    creator.run(ingest=True)
+    creator = CreateDwarfInjectionCatalog(config_dict)
+    creator.run(ingest=ingest_bool)
--- python/chrysomallos/synthpop/adopt_a_cat.py
+++ python/chrysomallos/synthpop/adopt_a_cat.py
@@ -1,15 +1,17 @@
-import artpop
 import astropy.units as u
-from astropy.table import Table
-from astropy.coordinates import SkyCoord
-from dustmaps.sfd import SFDQuery
 import numpy as np
-import pandas as pd
-from chrysomallos.utils import (totmag, totmag_below_maglim, fluxfrac_above_maglim,
-                            mag_at_flux_percentile, rad_physical_to_sky,
-                            mstar_from_absmag)
-from .artpop_source import MISTSersicSSPChrysomallos
+from astropy.coordinates import SkyCoord
+from astropy.table import Table
+from dustmaps.sfd import SFDQuery
+
+from chrysomallos.utils import (
+    mstar_from_absmag,
+    rad_physical_to_sky,
+    totmag_below_maglim,
+)
 from chrysomallos.utils.log import logger
+
+from .artpop_source import MISTSersicSSPChrysomallos
 
 __all__ = [
     "adopt_a_cat",
@@ -17,12 +19,23 @@ __all__ = [
 ]
 
 
-def adopt_a_cat(wcs, bbox,
-                age=10.0, feh=-2.0, mass=5.0e5, dist=2.0, r_scale=300.0,
-                ellip=0, theta=0, n=1, M_V=None,
-                mag_limit=36.0, mag_limit_band='LSST_g',
-		        random_seed=None, **kwargs
-                ):
+def adopt_a_cat(
+    wcs,
+    bbox,
+    age=10.0,
+    feh=-2.0,
+    mass=5.0e5,
+    dist=2.0,
+    r_scale=300.0,
+    ellip=0,
+    theta=0,
+    n=1,
+    M_V=None,
+    mag_limit=36.0,
+    mag_limit_band="LSST_g",
+    random_seed=None,
+    **kwargs,
+):
     """Make a synthetic source catalog to inject into an image.
 
     Parameters
@@ -55,7 +68,7 @@ def adopt_a_cat(wcs, bbox,
     mag_limit_band : `str`
         Band to apply mag_limit in
     random_seed: `int`
-	    if not None this sets the random seed to generate catalog with
+            if not None this sets the random seed to generate catalog with
 
     Returns
     -------
@@ -69,8 +82,8 @@ def adopt_a_cat(wcs, bbox,
     # Note: we have assume rscale = r_half. I _think_ this is true for the
     #   projected half-light radius and Plummer scale radius...
 
-    dist = dist*u.Mpc
-    r_scale = r_scale*u.pc
+    dist = dist * u.Mpc
+    r_scale = r_scale * u.pc
     pixel_scale = wcs.getPixelScale().asArcseconds()
     # If the image is 2k x 2k, the dwarf will be centered at 1000, 1000
     xydim = 1999
@@ -79,8 +92,8 @@ def adopt_a_cat(wcs, bbox,
         mass = mstar_from_absmag(M_V)
 
     # create the artpop stellar population
-    ssp = MISTSersicSSPStarlink(
-        log_age=np.log10(age*1e9),
+    ssp = MISTSersicSSPChrysomallos(
+        log_age=np.log10(age * 1e9),
         feh=feh,
         total_mass=mass,
         distance=dist,
@@ -88,10 +101,10 @@ def adopt_a_cat(wcs, bbox,
         ellip=ellip,
         theta=theta,
         n=n,
-        phot_system='LSST', # photometric system
-        imf='kroupa', # default imf
-        xy_dim=xydim, # half the size of an LSST patch
-        pixel_scale=pixel_scale, # pixel scale of input image
+        phot_system="LSST",  # photometric system
+        imf="kroupa",  # default imf
+        xy_dim=xydim,  # half the size of an LSST patch
+        pixel_scale=pixel_scale,  # pixel scale of input image
         mag_limit=mag_limit,
         mag_limit_band=mag_limit_band,
     )
@@ -103,22 +116,23 @@ def adopt_a_cat(wcs, bbox,
     radec_coords = wcs.pixelToSkyArray(xcoords, ycoords, degrees=True)
 
     # Add extinction
-    # see https://github.com/rubin-dp0/delegate-contributions-dp02/blob/db7d06ba6203faa15732c5368ff6e52ea53c5796/MWhalo_density/Milky_Way_halo_density.ipynb#L64
+    # see https://github.com/rubin-dp0/delegate-contributions-dp02/blob/db7d0\
+    # 6ba6203faa15732c5368ff6e52ea53c5796/MWhalo_density/Milky_Way_halo_\
+    # density.ipynb#L64
 
     # set the A_lamba/E(B-V) values for the six ugrizy LSST filters
     band_a_ebv = np.array([4.812, 3.643, 2.699, 2.063, 1.578, 1.313])
 
-    coords = SkyCoord(radec_coords[0], radec_coords[1],
-                      unit='deg', frame='icrs')
+    coords = SkyCoord(radec_coords[0], radec_coords[1], unit="deg", frame="icrs")
 
     sfd = SFDQuery()
     ebvvec = sfd(coords)
 
-    u_ext = ssp.mags['LSST_u'] + ebvvec*band_a_ebv[0]
-    g_ext = ssp.mags['LSST_g'] + ebvvec*band_a_ebv[1]
-    r_ext = ssp.mags['LSST_r'] + ebvvec*band_a_ebv[2]
-    i_ext = ssp.mags['LSST_i'] + ebvvec*band_a_ebv[3]
-    z_ext = ssp.mags['LSST_z'] + ebvvec*band_a_ebv[4]
+    u_ext = ssp.mags["LSST_u"] + ebvvec * band_a_ebv[0]
+    g_ext = ssp.mags["LSST_g"] + ebvvec * band_a_ebv[1]
+    r_ext = ssp.mags["LSST_r"] + ebvvec * band_a_ebv[2]
+    i_ext = ssp.mags["LSST_i"] + ebvvec * band_a_ebv[3]
+    z_ext = ssp.mags["LSST_z"] + ebvvec * band_a_ebv[4]
 
     # if band_for_injection == 'g':
     #     mag_for_injection = g_ext
@@ -131,20 +145,35 @@ def adopt_a_cat(wcs, bbox,
 
     dist_column = np.repeat(dist.value, len(ssp.mags))
 
-    cat = Table({'injection_id': np.arange(len(ssp.mags)),
-                 'ra': radec_coords[0], 'dec': radec_coords[1],
-                 'source_type': ['DeltaFunction']*len(ssp.mags),
-                 'distance': dist_column,
-                 'g_mag': g_ext, 'r_mag': r_ext, 'i_mag': i_ext,
-                 #'mag': mag_for_injection,
-                 })
+    cat = Table(
+        {
+            "injection_id": np.arange(len(ssp.mags)),
+            "ra": radec_coords[0],
+            "dec": radec_coords[1],
+            "source_type": ["DeltaFunction"] * len(ssp.mags),
+            "distance": dist_column,
+            "g_mag": g_ext,
+            "r_mag": r_ext,
+            "i_mag": i_ext,
+            # 'mag': mag_for_injection,
+        }
+    )
 
     return cat
 
 
-def massage_the_cat(cat_inp, mag_limit, band_for_injection,
-                    wcs, bbox, x_cen, y_cen,
-                    r_scale=300.0, dist=2.0, **kwargs):
+def massage_the_cat(
+    cat_inp,
+    mag_limit,
+    band_for_injection,
+    wcs,
+    bbox,
+    x_cen,
+    y_cen,
+    r_scale=300.0,
+    dist=2.0,
+    **kwargs,
+):
     """Replace the total flux below some mag limit with the
          appropriate Sersic model.
 
@@ -185,14 +214,16 @@ def massage_the_cat(cat_inp, mag_limit, band_for_injection,
     # Reduce the original catalog to only stars above the magnitude limit.
     # Append the Sersic model for the remaining flux.
 
-    band = band_for_injection+'_mag'
+    band = band_for_injection + "_mag"
 
     mag_for_sersic = totmag_below_maglim(cat_inp[band], mag_limit)
-    logger.info(f"massage a cat mag limit {mag_limit}, sersic component {mag_for_sersic} mag")
+    logger.info(
+        f"massage a cat mag limit {mag_limit}, sersic component {mag_for_sersic} mag"
+    )
     # Replicate the magnitude column for the band you want to inject into:
-    cat_inp['mag'] = cat_inp[band]
+    cat_inp["mag"] = cat_inp[band]
 
-    cat = cat_inp[cat_inp['mag'] <= mag_limit]
+    cat = cat_inp[cat_inp["mag"] <= mag_limit]
 
     # Parameters for simulated dwarf:
     # r_scale = r_scale*u.pc
@@ -205,15 +236,15 @@ def massage_the_cat(cat_inp, mag_limit, band_for_injection,
     # dec_sim = np.median(cat['dec'])
 
     # Shift the input coordinates to the desired xy coord:
-    xycoords_inp = wcs.skyToPixelArray(cat['ra'], cat['dec'], degrees=True)
+    xycoords_inp = wcs.skyToPixelArray(cat["ra"], cat["dec"], degrees=True)
     # Using a fixed injection image size of 2000x2000 for now
     dwarfcen_x = 1000
     dwarfcen_y = 1000
     xxx = xycoords_inp[0] - dwarfcen_x + x_cen
     yyy = xycoords_inp[1] - dwarfcen_y + y_cen
     radec_coords_stars = wcs.pixelToSkyArray(xxx, yyy, degrees=True)
-    cat['ra'] = radec_coords_stars[0]
-    cat['dec'] = radec_coords_stars[1]
+    cat["ra"] = radec_coords_stars[0]
+    cat["dec"] = radec_coords_stars[1]
 
     # Append a single line for the "galaxy" that contains the unresolved flux:
     cat.add_row()
@@ -224,27 +255,28 @@ def massage_the_cat(cat_inp, mag_limit, band_for_injection,
     ycoord = y_cen + y0
     radec_coords = wcs.pixelToSky(float(xcoord), float(ycoord))
 
-    cat[-1]['ra'] = radec_coords[0].asDegrees()
-    cat[-1]['dec'] = radec_coords[1].asDegrees()
-    cat[-1]['mag'] = mag_for_sersic
-    try:
-        cat[-1]['source_type'] = 'Sersic'
-    except:
-        import pdb; pdb.set_trace()
+    cat[-1]["ra"] = radec_coords[0].asDegrees()
+    cat[-1]["dec"] = radec_coords[1].asDegrees()
+    cat[-1]["mag"] = mag_for_sersic
+    cat[-1]["source_type"] = "Sersic"
+
     # I think instead of including distance, I need to convert the radius
     # into a sky radius instead of physical. CHECK THIS!
-    cat[-1]['distance'] = dist
+    cat[-1]["distance"] = dist
 
-    # We need Sersic index, position angle, axis ratio, and semimajor axis for the galaxy model,
-    #   so create columns for these (with all stars set to some default values)
-    semimajor_all = 0.0*cat['mag']
+    # We need Sersic index, position angle, axis ratio, and semimajor axis for
+    # the galaxy model,
+    #  so create columns for these (with all stars set to some default values)
+    semimajor_all = 0.0 * cat["mag"]
     semimajor_all[-1] = radius
-    sersic_n_all = 0.0*cat['mag'] + 1.0
+    sersic_n_all = 0.0 * cat["mag"] + 1.0
     sersic_n_all[-1] = 1.0
-    pa_all = 0.0*cat['mag']
-    axis_ratio_all = np.ones(len(cat['mag']))
+    pa_all = 0.0 * cat["mag"]
+    axis_ratio_all = np.ones(len(cat["mag"]))
 
-    cat.add_columns([semimajor_all, sersic_n_all, pa_all, axis_ratio_all],
-                    names=['half_light_radius', 'n', 'pa', 'axis_ratio'])
+    cat.add_columns(
+        [semimajor_all, sersic_n_all, pa_all, axis_ratio_all],
+        names=["half_light_radius", "n", "pa", "axis_ratio"],
+    )
 
     return cat
