a8b47e9a711172558c6381dbfbffe3e86b45f282
Jeff Carlin <jcarlin@lsst.org>
2024-02-05 13:31:13 -0800

    Working unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 
 from chrysomallos.utils import dist_to_dmod, sdss_g_to_V, totmag
+from chrysomallos.injection import adopt_a_cat
 
 import lsst.geom as geom
 import lsst.afw.geom as afwGeom
@@ -10,7 +11,7 @@ import lsst.afw.geom as afwGeom
 
 class MVTest(unittest.TestCase):
     def test_mv_application(self):
-        mv_vals = np.arange(-12, -8, 0.5)
+        mv_vals = np.arange(-10, -8, 0.5)
 
         # Confirm that passing each of these M_V values to the
         # sythetic catalog generation code returns the correct
@@ -31,15 +32,14 @@ class MVTest(unittest.TestCase):
         MV_out = []
         for m_v in mv_vals:
             cat = adopt_a_cat(
-                wcs, bbox, age=10.0, feh=-2.0, dist=1.0,
+                wcs_fake, bbox_fake, age=10.0, feh=-2.0, dist=1.0,
                 r_scale=100.0, ellip=0, theta=0, n=1,
                 m_v=m_v, mag_limit=36.0, mag_limit_band="LSST_g",
-                random_seed=None, **kwargs
+                random_seed=None,
             )
             vmags = sdss_g_to_V(cat['g_mag'], cat['r_mag'])
-            MV_out.append(totmag(vmags)-dist_to_dmod(distance*1.0e6))
-
-        self.assertAlmostEqual(list(mv_vals), MV_out, delta=0.2)
+            MV_out = totmag(vmags)-dist_to_dmod(distance*1.0e6)
+            self.assertAlmostEqual(m_v, MV_out, delta=0.2)
 
 if __name__ == "__main__":
     unittest.main()
